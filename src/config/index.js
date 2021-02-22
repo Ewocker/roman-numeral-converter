7d760d9c61051c61523bbc67efbe3f5a719bd636
Ewocker <chockerlin@gmail.com>
2021-02-21 18:02:11 -0800

    add k8s resource spec

--- src/config/index.js
+++ src/config/index.js
@@ -1,13 +1,23 @@
 const configs = {
-	local: {
+	'local': {
+  	redisURL: 'redis://127.0.0.1:6379',
+		enableRedisCache: false,
+		cacheSyncInterval: 5000
+	},
+	'local-redis': {
   	redisURL: 'redis://127.0.0.1:6379',
 		enableRedisCache: true,
 		cacheSyncInterval: 5000
 	},
-	production: {
+	'docker-compose': {
   	redisURL: 'redis://127.0.0.1:6379',
-		enableRedisCache: false,
-		cacheSyncInterval: 5000
+		enableRedisCache: true,
+		cacheSyncInterval: 60000 // 1 minute
+	},
+	'kubernetes': {
+  	redisURL: 'redis://redis-cache:6379',
+		enableRedisCache: true,
+		cacheSyncInterval: 60000 // 1 minute
 	}
 }
 
